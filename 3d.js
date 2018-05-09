aac60a552390fe4fee2013d1aece2c9f945f5fb2
Christof Schwarz <christof.schwarz@gmx.at>
2018-05-09 17:52:42 +0200

    v1

--- 3d.js
+++ 3d.js
@@ -1,30 +1,4 @@
-/*
-function clickqbottom() {
- 	document.getElementById('QBOTTOM').style.transform='rotateX(90deg) translate3d(0, 0, 0)';
-	document.getElementById('QLEFT').style.transform='rotateX(90deg) translate3d(0, 0, -225px)';
-	document.getElementById('QRIGHT').style.transform='rotateX(90deg) translate3d(0, 0, -225px)';	
-}
 
-function clickqleft() {
- 	document.getElementById('QBOTTOM').style.transform='rotateX(90deg) translate3d(0, 0, -225)';
-	document.getElementById('QLEFT').style.transform='rotateX(90deg) translate3d(0, 0, 0)';
-	document.getElementById('QRIGHT').style.transform='rotateX(90deg) translate3d(0, 0, -225px)';	
-}
-
-function clickqright() {
- 	document.getElementById('QBOTTOM').style.transform='rotateX(90deg) translate3d(0, 0, -225)';
-	document.getElementById('QLEFT').style.transform='rotateX(90deg) translate3d(0, 0, -225px)';
-	document.getElementById('QRIGHT').style.transform='rotateX(90deg) translate3d(0, 0, 0)';	
-}
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-*/
 /*
  * Basic responsive mashup template
  * @owner Enter you name here (xxx)
@@ -60,8 +34,8 @@ require( ["js/qlik"], function ( qlik ) {
 	app.getObject('QLEFT','qTCxpE'); //bgBDjcJ');
 	app.getObject('QRIGHT','PzSZaU');
 	app.getObject('QBOTTOM','kpqpaT');
-	app.getObject('QV07', 'dNrEW');
-	app.getObject('QV08', 'QhjBZR');
+	app.getObject('QSIDE1', 'dNrEW');
+	app.getObject('QSIDE2', 'QhjBZR');
 	
 	app.getObject('CurrentSelections','CurrentSelections');
 	//create cubes and lists -- inserted here --
